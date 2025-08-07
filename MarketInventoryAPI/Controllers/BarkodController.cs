8baf921c68f831f6f06365295ee2ac2b40b6cd92
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-08-07 16:34:36 +0300

    Update BarkodController.cs

--- MarketInventoryAPI/Controllers/BarkodController.cs
+++ MarketInventoryAPI/Controllers/BarkodController.cs
@@ -22,6 +22,7 @@ public class BarkodController : ControllerBase
     public async Task<IActionResult> GetById(int id)
     {
         var item = await _barkodService.GetByIdAsync(id);
+        
         return item == null ? NotFound() : Ok(item);
     }
 
@@ -30,13 +31,16 @@ public class BarkodController : ControllerBase
     public async Task<IActionResult> Create([FromBody] Barkod barkod)
     {
         await _barkodService.AddAsync(barkod);
+        
         return NoContent();
     }
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(int id, [FromBody] Barkod barkod)
     {
         if (id != barkod.Id) return BadRequest();
+        
         await _barkodService.UpdateAsync(barkod);
+        
         return NoContent();
     }
 
@@ -44,8 +48,11 @@ public class BarkodController : ControllerBase
     public async Task<IActionResult> Delete(int id)
     {
         var barkod = await _barkodService.GetByIdAsync(id);
+        
         if (barkod == null) return NotFound();
+        
         await _barkodService.DeleteAsync(barkod);
+        
         return NoContent();
     }
 }
