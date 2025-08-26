5e05c08cc84988784a5ac871354a036dd39d5e8d
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-08-26 17:03:42 +0300

    Update LoginDto.cs

--- MarketInventory.Application/Dtos/LoginDto.cs
+++ MarketInventory.Application/Dtos/LoginDto.cs
@@ -6,6 +6,7 @@ namespace MarketInventory.Application.Dtos
     public class LoginDto
     {
         public string KullaniciAdi { get; set; } = string.Empty;
+        
         public string Password { get; set; } = string.Empty;
     }
 }
