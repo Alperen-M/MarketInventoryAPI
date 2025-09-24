f6585e18acf2ccd4937f5cee3290248c937f69ad
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-09-24 14:08:36 +0300

    Update Barkod.cs

--- MarketInventoryAPI/MarketInventory.Domain/Entities/Barkod.cs
+++ MarketInventoryAPI/MarketInventory.Domain/Entities/Barkod.cs
@@ -7,9 +7,11 @@ public class Barkod
     public string Kod { get; set; } = string.Empty;
 
     public int? UrunId { get; set; }
+    
     public Urun? Urun { get; set; }
 
     public int UrunBirimId { get; set; }
+    
     public Birim? UrunBirimi { get; set; }
 
     public bool AktifMi { get; set; } = true;
