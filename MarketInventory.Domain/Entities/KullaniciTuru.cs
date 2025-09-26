e6d448d82e5605cc2673879622d8f7a1decebad3
Alperen Mengünoğul <alperenmengunogul1@gmail.com>
2025-09-26 18:40:53 +0300

    Update KullaniciTuru.cs

--- MarketInventory.Domain/Entities/KullaniciTuru.cs
+++ MarketInventory.Domain/Entities/KullaniciTuru.cs
@@ -3,6 +3,5 @@ public class KullaniciTuru
 {
     public int Id { get; set; }
     public string Ad { get; set; } = string.Empty;
-
     public ICollection<Kullanici> Kullanicilar { get; set; } = new List<Kullanici>();
 }
